a97398618682856a63790cf17b16aef45744b3e2
Benjamin Dengler <benjamin.dengler@gmail.com>
2025-07-13 10:11:09 +0200

    Made update-wallpaper.sh runnable as non-root user

--- update-wallpaper.sh
+++ update-wallpaper.sh
@@ -7,17 +7,32 @@ USER=$(ls /home)
 SAVE_PATH="/home/$USER/Pictures/wallpaper.png"
 RETRY_DELAY=5
 
+# Try downloading wallpaper until it succeeds
 while true; do
     wget -q -N -O "$SAVE_PATH" "$IMAGE_URL"
-    if [ $? -eq 0 ]; then
+    if [[ $? -eq 0 ]]; then
         break
     else
         sleep $RETRY_DELAY
     fi
 done
 
-export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$(id -u $USER)/bus"
-runuser -u $USER -- /usr/bin/plasma-apply-wallpaperimage $SAVE_PATH
-runuser -u $USER -- kwriteconfig6 --file kscreenlockerrc --group Greeter --group Wallpaper --group org.kde.image --group General --key Image "file://$SAVE_PATH"
+# Update wallpaper
+if [[ $EUID -ne 0 ]]; then
+    /usr/bin/plasma-apply-wallpaperimage $SAVE_PATH
+else
+    export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$(id -u $USER)/bus"
+    runuser -u $USER -- /usr/bin/plasma-apply-wallpaperimage $SAVE_PATH
+fi
 
-cp $SAVE_PATH /usr/share/sddm/themes/breeze/wallpaper.png || true
+# Update lock screen wallpaper
+if [[ $EUID -ne 0 ]]; then
+    kwriteconfig6 --file kscreenlockerrc --group Greeter --group Wallpaper --group org.kde.image --group General --key Image "file://$SAVE_PATH"
+else
+    runuser -u $USER -- kwriteconfig6 --file kscreenlockerrc --group Greeter --group Wallpaper --group org.kde.image --group General --key Image "file://$SAVE_PATH"
+fi
+
+# Update SDDM wallpaper
+if [[ $EUID -eq 0 ]]; then
+    cp $SAVE_PATH /usr/share/sddm/themes/breeze/wallpaper.png || true
+fi
